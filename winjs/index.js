496a4af5f3fefad3cc431056af5c2352f6091f4e
Chris Anderson <chrisan@outlook.com>
2015-04-27 08:38:24 -0700

    contentdialog in winjs section

--- winjs/index.js
+++ winjs/index.js
@@ -23,7 +23,28 @@
             blockSlider.max = "" + (window.unicode.blocks.length - 1);
             blockSlider.addEventListener("change", update);
             update();
+
+            content.addEventListener('iteminvoked', handleListViewItemInvoked);
         });
     };
 
+    function showDialog(data) {
+        var heading = document.querySelector(".win-contentdialog .heading");
+        var body = document.querySelector(".win-contentdialog .body");
+        heading.innerHTML = data.preview;
+        body.textContent = data.name;
+        document.querySelector(".win-contentdialog").winControl.show();
+    }
+
+    function cancelDismissal(evenObject) {
+        if (evenObject.detail.result === WinJS.UI.ContentDialog.DismissalResult.none) {
+            evenObject.preventDefault();
+        }
+    }
+
+    function handleListViewItemInvoked (ev) {
+        ev.detail.itemPromise.then(function (item) {
+            showDialog(item.data);
+        })
+    }
 })();
